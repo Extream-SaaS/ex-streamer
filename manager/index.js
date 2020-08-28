13b4513f9c4e94d93dbfe7274f7d71c0063a5113
Rich Wilson <richard.wilson@smgdesign.org>
2020-08-28 02:33:09 +0100

    added promise.all

--- manager/index.js
+++ manager/index.js
@@ -52,8 +52,10 @@ exports.manage = async (event, context, callback) => {
           addedAt: Firestore.FieldValue.serverTimestamp()
         });
         // send a message to the manager to add this itinerary item to the itinerary
-        await publish('ex-manage', { domain, action, command, payload: { ...payload, id: docRef.id }, user, socketId });
-        await publish('ex-gateway', { domain, action, command, payload: { ...payload, id: docRef.id }, user, socketId });
+        await Promise.all([
+          publish('ex-manage', { domain, action, command, payload: { ...payload, id: docRef.id }, user, socketId }),
+          publish('ex-gateway', { domain, action, command, payload: { ...payload, id: docRef.id }, user, socketId })
+        ]);
         callback();
       } catch (error) {
         await publish('ex-gateway', { error: error.message, domain, action, command, payload, user, socketId });
