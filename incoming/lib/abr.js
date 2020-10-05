f50411f692d794aeb4cf0ec57549b2f240335c1e
Rich Wilson <richard.wilson@smgdesign.org>
2020-10-05 21:37:00 +0100

    added logging back and disabled 720p to prove quality

--- incoming/lib/abr.js
+++ incoming/lib/abr.js
@@ -3,7 +3,7 @@ const fs = require('./fs');
 const abrTemplate = () => {
   let line = `#EXTM3U\n#EXT-X-VERSION:3\n`;
   line += `#EXT-X-STREAM-INF:BANDWIDTH=1400000,RESOLUTION=842x480\n480p/index.m3u8\n`;
-  line += `#EXT-X-STREAM-INF:BANDWIDTH=2800000,RESOLUTION=1280x720\n720p/index.m3u8`;
+  // line += `#EXT-X-STREAM-INF:BANDWIDTH=2800000,RESOLUTION=1280x720\n720p/index.m3u8`;
   return line;
 };
 
