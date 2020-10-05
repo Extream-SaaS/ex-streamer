5863dd0d978a1f9ab32e126a7fe30889fad33991
Rich Wilson <richard.wilson@smgdesign.org>
2020-10-05 19:53:58 +0100

    removed lower res from abr

--- incoming/lib/abr.js
+++ incoming/lib/abr.js
@@ -2,7 +2,6 @@ const fs = require('./fs');
 
 const abrTemplate = () => {
   let line = `#EXTM3U\n#EXT-X-VERSION:3\n`;
-  line += `#EXT-X-STREAM-INF:BANDWIDTH=800000,RESOLUTION=640x360\n360p/index.m3u8\n`;
   line += `#EXT-X-STREAM-INF:BANDWIDTH=1400000,RESOLUTION=842x480\n480p/index.m3u8\n`;
   line += `#EXT-X-STREAM-INF:BANDWIDTH=2800000,RESOLUTION=1280x720\n720p/index.m3u8`;
   return line;
